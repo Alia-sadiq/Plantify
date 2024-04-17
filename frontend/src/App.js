b3291e92832e236fa7cdce97e49d448b2bcf4342
AliaSadiq <aliasadiq2003@gmail.com>
2024-04-17 18:42:37 +0500

    dashboard added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,28 @@
-//import logo from './logo.svg';
-import './App.css';
-import NavigationBar from './NavigationBar';
-
+import React from "react";
+import "./App.css";
+import "./index.css";
+import Dashboard from "./pages/dashboard";
+import inbox from "./pages/inbox";
+import create from "./pages/create";
+import team from "./pages/team";
+import setting from "./pages/setting";
+import profile from "./pages/profile";
+import Sidebar from "./components/sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <NavigationBar/>
-    </div>
+    <Router>
+      <Sidebar />
+  
+      <Routes>
+        <Route index element={<Dashboard />} />
+        <Route path="/pages/inbox" element={<inbox />} />
+        <Route path="/pages/create" element={<create />} />
+        <Route path="/pages/team" element={<team />} />
+        <Route path="/pages/setting" element={<setting />} />
+        <Route path="/pages/profile" element={<profile />} />
+      </Routes>
+    </Router>
   );
 }
 
